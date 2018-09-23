e7c32cbcb82e3e4277a5c487f98f84c3176b64e6
rifkegribenes <rifkegribenes@gmail.com>
2018-09-23 12:45:41 -0700

    server boilerplate

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+const dotenv = require('dotenv').load();
+
+// connect to db
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const configDB = require('./app/config/database.js');
+mongoose.connect(configDB.url, configDB.options);
+mongoose.Promise = global.Promise;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'});
+});
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage);
+});
+
+const listener = app.listen(process.env.PORT || 3001, () => {
+  console.log(`Node is listening on port ${listener.address().port}`);
+});
